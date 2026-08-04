240561675a3f0999c0a5f9f9d0251143e8a4c6d7
Alithiel31 <harmo.man.01@gmail.com>
2026-08-04 20:23:06 +0200

    feat : add more cities in the homepage

--- backend/src/data/cities.ts
+++ backend/src/data/cities.ts
@@ -21,4 +21,32 @@ export const CITIES: Record<string, Ville> = {
     longitude: -71.2075,
     timezone: 'America/Toronto',
   },
+  gatineau: {
+    id: 'gatineau',
+    nom: 'Gatineau',
+    latitude: 45.4765,
+    longitude: -75.7013,
+    timezone: 'America/Toronto',
+  },
+  sherbrooke: {
+    id: 'sherbrooke',
+    nom: 'Sherbrooke',
+    latitude: 45.4042,
+    longitude: -71.8929,
+    timezone: 'America/Toronto',
+  },
+  'trois-rivieres': {
+    id: 'trois-rivieres',
+    nom: 'Trois-Rivières',
+    latitude: 46.3432,
+    longitude: -72.5432,
+    timezone: 'America/Toronto',
+  },
+  saguenay: {
+    id: 'saguenay',
+    nom: 'Saguenay',
+    latitude: 48.428,
+    longitude: -71.0685,
+    timezone: 'America/Toronto',
+  },
 };
